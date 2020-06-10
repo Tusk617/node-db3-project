4431c19bca471eef7074cd4947e619712295630e
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-10 13:40:20 -0400

    Created findById function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,15 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
-    find
+    find,
+    findById
 };
 
 
 function find() {
     return db('schemes');
+}
+
+function findById(id) {
+    return db("schemes").where({id}).first();
 }
