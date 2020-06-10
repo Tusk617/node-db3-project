cc68b263389b327bc166a2c963ba5a64efbfc502
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-10 15:56:22 -0400

    Started working on findbyid function, still needs work. But got the add function working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,14 +4,27 @@ const db = knex(knexConfig.development);
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps,
+    add
 };
 
 
 function find() {
     return db('schemes');
-}
+}//working
 
 function findById(id) {
     return db("schemes").where({id}).first();
+}//working
+
+function findSteps(id) {
+    return db("steps")
+    .join("schemes", "steps.schemes_id", "=", "schemes.id")
+    .select("schemes.scheme_name", "steps.*");
+}
+
+function add(scheme) {
+    return db("schemes").insert(scheme)
+    .then(ids => ({id: ids[0 ]}))
 }
